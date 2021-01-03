8a2ba74f2a25a98699932a06be0fc12f2525aa4b
felipendc <felipendc10@gmail.com>
2021-01-03 00:19:33 -0300

    Using Arcolinux-ISO build structure. 2021-01-03_00:18:39

---
+++ archiso/profiledef.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name=
+iso_label=
+iso_publisher=""
+iso_application=""
+iso_version=
+
+install_dir=arch
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
+#airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+airootfs_image_tool_options=('-comp' 'xz')
+file_permissions=(
+  ["/etc/shadow"]="0:0:600"
+  ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/etc/polkit-1/rules.d"]="0:0:750"
+  ["/etc/sudoers.d"]="0:0:750"
+  ["/usr/local/bin/arcolinux-all-cores.sh"]="0:0:755"
+  ["/usr/local/bin/arcolinux-before.sh"]="0:0:755"
+  ["/usr/local/bin/arcolinux-final.sh"]="0:0:755"
+)
