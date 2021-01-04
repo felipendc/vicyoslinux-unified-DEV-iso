b000551f46fdcfb0d5947fcb3cccc7a116e73d2b
Vicyos <felipendc10phoenix@gmail.com>
2021-01-04 01:59:47 -0300

    Do not rename vicyos-release to lsb-release

--- archiso/airootfs/usr/local/bin/arcolinux-final.sh
+++ archiso/airootfs/usr/local/bin/arcolinux-final.sh
@@ -41,7 +41,7 @@ rm -f /etc/polkit-1/rules.d/49-nopasswd_global.rules
 rm -r /etc/systemd/system/etc-pacman.d-gnupg.mount
 rm /root/{.automated_script.sh,.zlogin}
 rv -R /etc/lsb-release
-mv /etc/vicyos-release /etc/lsb-release
+#mv /etc/vicyos-release /etc/lsb-release
 
 echo "Permission of root"
 echo "#################################"
